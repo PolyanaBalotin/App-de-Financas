b9c1f3fab37c3ee3475b179cc5d347e17239f8ce
Polyana Balotin <polybalotin@hotmail.com>
2023-09-20 15:58:29 -0300

    Add Movements

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
 import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto conta de luz',
+    value: '300,00',
+    date: '17/09/2022',
+    type: 0
+  },
+  {
+    id: 2,
+    label: 'Pix Cliente',
+    value: '350,00',
+    date: '17/09/2022',
+    type: 1
+  },
+  {
+    id: 3,
+    label: 'Salário',
+    value: '5.000,00',
+    date: '17/09/2022',
+    type: 1
+  }
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name='Polyana' />
+
       <Balance saldo='23.567,00' gastos='-4.244,00' />
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={ ({ item }) => <Movements data={item} /> }
+      />
+
     </View>
   );
 }
@@ -16,4 +53,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'aliceblue',
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
+  }
 });
